e3c921b210be283f46c210a0f7d52dea62658461
f1shh <thethirdepic@foxmail.com>
2019-04-09 14:47:40 +0800

    fixed cors

--- app/backend/lctf2018_backend/settings.py
+++ app/backend/lctf2018_backend/settings.py
@@ -27,10 +27,10 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOW_CREDENTIALS = False
 CORS_ORIGIN_WHITELIST = (
-    '*'
+
 )
 
 # Application definition
